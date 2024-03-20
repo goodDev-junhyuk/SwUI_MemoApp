2428407ac58b45bdcc45c47b33b22050e094de57
Jang Jun Hyuk <dejavukk1@gmail.com>
2024-03-20 17:13:53 +0900

    Update ComposeView.swift
    Step5. 글쓰기 기능 구현

--- SwUI_MemoApp/View/ComposeView.swift
+++ SwUI_MemoApp/View/ComposeView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct ComposeView: View {
+    
+    @EnvironmentObject var store: MemoStore
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var content: String = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            VStack {
+                TextEditor(text: $content)
+                    .padding()
+                
+            }
+            .navigationTitle("새로운 메모")
+            
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Button {
+                        
+                        store.insert(memo: content)
+                        
+                        dismiss()
+                    } label: {
+                        Text("취소")
+                    }
+                }
+                
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("저장")
+                    }
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    ComposeView()
+
+struct ComposeView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        ComposeView()
+            .environmentObject(MemoStore())
+    }
 }
