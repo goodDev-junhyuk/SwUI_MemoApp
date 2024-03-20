f3e155bbb68686bd06880751172c965d381105ee
Jang Jun Hyuk <dejavukk1@gmail.com>
2024-03-20 13:08:53 +0900

    Update Memo.swift

--- SwUI_MemoApp/Memory/Memo.swift
+++ SwUI_MemoApp/Memory/Memo.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class Memo: Identifiable, ObservableObject {
+    
+    let id: UUID
+    @Published var content: String
+    
+    let insertDate: Date
+    
+    init(content: String, inserDate: Date = Date.now) {
+        
+        id = UUID()
+        self.content = content
+        self.insertDate = inserDate
+        
+        
+    }
+}
