3babe77bb22d6112faa1f31ce9fcec0a89a86c82
Jang Jun Hyuk <dejavukk1@gmail.com>
2024-03-20 17:08:12 +0900

    Step5. 글쓰기 기능 구현

---
+++ SwUI_MemoApp/View/ComposeView.swift
@@ -0,0 +1,18 @@
+//
+//  ComposeView.swift
+//  SwUI_MemoApp
+//
+//  Created by junhyuk on 3/20/24.
+//
+
+import SwiftUI
+
+struct ComposeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ComposeView()
+}
