b00c47febe5e82aabbd6641e4b86e33f8277e809
Jang Jun Hyuk <dejavukk1@gmail.com>
2024-03-21 13:57:41 +0900

    Update MainListView.swift

--- SwUI_MemoApp/View/MainListView.swift
+++ SwUI_MemoApp/View/MainListView.swift
@@ -7,22 +7,43 @@
 
 import SwiftUI
 
+
 struct MainListView: View {
     
     @EnvironmentObject var store: MemoStore
+    
+    @State private var showComposer: Bool = false
     
     var body: some View {
         NavigationView {
             List(store.list) { memo in
                 
-                MemoCell(memo: memo)
-                
+                NavigationLink{
+                    
+                    DetailView(memo: memo)
+                } label: {
+                    
+                    MemoCell(memo: memo)
+                }
             }
             .listStyle(.plain)
             .navigationTitle("나의 메모")
+            .toolbar {
+                Button {
+                    showComposer = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+                
+            }
+            // 쓰기 화면 Modal방식으로 구현
+            .sheet(isPresented: $showComposer) {
+                ComposeView()
+            }
         }
     }
 }
+
 
 struct MainListView_Previews: PreviewProvider {
     
